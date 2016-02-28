f39cc0fdc55671ba90da1471235f00958cc28ccd
vignesh-m <vicky.mano@gmail.com>
2016-02-28 11:14:31 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 """ Main driver """
 
-from maxflow.dinic import DinicImageSequence,BlockingFlowImageSequence
+from maxflow.dinic import DinicImageSequence
 from maxflow.graph import input_graph, display_graph
-from gui.image_display import start_gui, ImageSequence
+from gui.image_display import start_gui
 
 graph = input_graph()
-display_graph(graph)
-# imageSequence = ImageSequence(graph)
 imageSequence = DinicImageSequence(graph, source=0, sink=1)
-# imageSequence = BlockingFlowImageSequence(graph, source=0, sink=1, dist=[])
 start_gui(imageSequence)

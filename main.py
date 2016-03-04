598a76a99e6515cf62b49f35d5bdc5814528215b
vignesh-m <vicky.mano@gmail.com>
2016-03-04 22:42:11 +0530

    basic bf display done

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ print graph
 
 def blocking_flow_test():
     b = BlockingFlowImageSequence(graph, v, e, [], 0, v - 1)
-    block_graph = b.blocking_flow()
-    print "blocking flow", block_graph
-    display_graph(block_graph, "Block_flow")
+    start_gui(b)
+    # block_graph = b.blocking_flow()
+    # print "blocking flow", block_graph
+    # display_graph(block_graph, "Block_flow")
 blocking_flow_test()

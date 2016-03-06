d21e3b5229103d3a2b79584ee518aaed4db331df
vignesh-m <vicky.mano@gmail.com>
2016-03-06 16:40:39 +0530

    uses python queue with O(1) push,pop

--- maxflow/dinic.py
+++ maxflow/dinic.py
@@ -5,31 +5,22 @@ import matplotlib.image as mpimg
 from graph import display_graph
 import random
 import numpy as np
+from Queue import Queue
 
 INF = float('inf') # use double inf
-
-class Queue:
-    def __init__(self):
-        self.nodes = []
-    def isempty(self):
-        return self.nodes == []
-    def enqueue(self,node):
-        self.nodes.insert(0,node)
-    def dequeue(self):
-        return self.nodes.pop()
 
 def find_distances(graph, source):
     """ Find distances using bfs """
     dist = [INF for _ in graph]
     q = Queue()
     dist[source]=0
-    q.enqueue(source)
-    while(q.isempty()==0):
-        current=q.dequeue()
+    q.put(source)
+    while(q.empty()==0):
+        current=q.get()
         for t in graph[current]:
             if dist[t[0]]==INF:
                 dist[t[0]]=dist[current]+1
-                q.enqueue(t[0])
+                q.put(t[0])
     return dist
 
 

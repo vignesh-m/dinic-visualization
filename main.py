0f03fcf2ee7bcd3527a55987421f2ffc95ba08d5
vignesh-m <vicky.mano@gmail.com>
2016-03-06 16:44:46 +0530

    bfs test, minor changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """ Main driver """
 
-from maxflow.dinic import DinicImageSequence
+from maxflow.dinic import DinicImageSequence, find_distances
 from maxflow.graph import input_graph, display_graph
 from maxflow.block_flow import BlockingFlowImageSequence
 from gui.image_display import start_gui
@@ -16,27 +16,29 @@ def dinic_test():
 
 
 def blocking_flow_test():
-    b = BlockingFlowImageSequence(graph, v, e,[], 0, v - 1)
+    b = BlockingFlowImageSequence(graph, v, e, [], 0, v - 1)
     block_graph = b.blocking_flow()
     print "blocking flow", block_graph
     display_graph(block_graph, "Block_flow")
 
-def residual_test():
 
+def residual_test():
     graph_zero_wt = [[] for i in range(v)]
 
     for i in range(v):
         for j in range(len(graph[i])):
             temp1, temp2 = graph[i][j]
-            graph_zero_wt[i].append((temp1, temp2-2))
-    
+            graph_zero_wt[i].append((temp1, temp2 - 2))
+
     b = DinicImageSequence(graph_zero_wt, v, e, 0, v - 1)
     res_graph = b.residual(graph)
 
-    display_graph(res_graph,"residual_graph")
+    display_graph(res_graph, "residual_graph")
 
 
+def bfs_test():
+    print find_distances(graph, 0)
 
+# bfs_test()
 # dinic_test()
 residual_test()
-
